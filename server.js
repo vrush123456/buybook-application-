baaeb9f8a0eb56c87315518c6f503164517cd488
vrushabh vijay chandanshive <vijay.vrushabh@walchandsangli.ac.in>
2022-02-07 22:46:42 +0530

    add cart page,auth

--- server.js
+++ server.js
@@ -12,13 +12,31 @@ const PORT=process.env.PORT||3000
 //assets
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
-    res.render('home.ejs')
-})
+
 // set template engine
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.render('home.ejs')
+})
+
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
+
+app.get('/contact-us',(req,res)=>{
+    res.render('auth/contact-us')
+})
 
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`)
